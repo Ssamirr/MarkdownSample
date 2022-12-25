5e6094cb937a70becee56f65019aa31b76cf2f69
Samir <ssardarlisamir@gmail.com>
2022-12-25 16:51:05 +0400

    css

--- src/App.js
+++ src/App.js
@@ -8,14 +8,14 @@ import Preview from './pages/Preview';
 function App() {
   const [text, setText] = useState("");
   return (
-    <>
+    <div className='main'>
       <Header />
 
       <Routes>
         <Route path="/" element={<Edit text={text} setText={setText} />} />
         <Route path="/preview" element={<Preview text={text} setText={setText} />} />
       </Routes>
-    </>
+    </div>
 
 
   );
--- src/pages/Preview.js
+++ src/pages/Preview.js
@@ -1,12 +1,37 @@
 import React from 'react'
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 function Preview(props) {
+
     return (
-        <>
-            <ReactMarkdown children={props.text} />
-        </>
-    )
+        <div className="preview">
+          <ReactMarkdown
+            children={props.text}
+            components={{
+              code({ node, inline, className, children, ...props }) {
+                const match = /language-(\w+)/.exec(className || "");
+                return !inline && match ? (
+                  <SyntaxHighlighter
+                    children={String(children).replace(/\n$/, "")}
+                    style={docco}
+                    language={match[1]}
+                    PreTag="div"
+                    {...props}
+                  />
+                ) : (
+                  <code className={className} {...props}>
+                    {children}
+                  </code>
+                );
+              },
+            }}
+          />
+        </div>
+      );
 }
+
+
 
 export default Preview
